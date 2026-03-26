1073efb5d2eb332afe4d1c356125702e896bb5ab
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2026-03-26 22:57:34 +0530

    Try to solve find the factorial of n number using recursion method by some different way

--- Factorialofnumber.java
+++ Factorialofnumber.java
@@ -1,21 +1,17 @@
-//it return the number from n to 1 using recursion
 import java.util.Scanner;
 public class Factorialofnumber {
-    public static void printfact(int i,int num,int fact){
-        if(i==num){
-            fact*=i;
-            System.out.println("Sum is "+fact);
-            return;
+    public static int printFact(int num){
+        if(num==1){
+            return 1;
         }
-        fact*=i;
-        printfact(i+1,num,fact);
+       return num*printFact(num-1);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num=sc.nextInt();
         
-        printfact(1,num,1);
+        System.out.println("Factorial of the number is "+printFact(num));
         
     }
 }
